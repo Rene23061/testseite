309609f2bb8d054494d4cdbd0dfa025f821bb8cb
Rene23061 <137315534+Rene23061@users.noreply.github.com>
2025-02-17 12:18:06 +0100

    config.py aktualisieren

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv("config.env")  # Lädt jetzt config.env statt .env
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT")
+}
