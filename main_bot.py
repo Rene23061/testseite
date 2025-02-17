42197a2095674d30bbe624bb3a9b4b9c93157748
Rene23061 <137315534+Rene23061@users.noreply.github.com>
2025-02-17 17:58:57 +0100

    main_bot.py aktualisieren

--- main_bot.py
+++ main_bot.py
@@ -31,7 +31,7 @@ def main():
     """Startet den Telegram-Bot."""
     app = Application.builder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("termin", termin))
     app.add_handler(CallbackQueryHandler(button_handler))
     
     # Einzel- und Event-Buchungen registrieren
