21501e310a7ff08f897ee557121ab8d7c6be2218
Rene23061 <137315534+Rene23061@users.noreply.github.com>
2025-02-18 15:18:00 +0100

    config.py aktualisieren

--- config.py
+++ config.py
@@ -1,15 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-# Lade config.env aus /root/eventbot/
-load_dotenv("/root/eventbot/config.env")
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT")
-}
+BOT_TOKEN = "DEIN_TELEGRAM_BOT_TOKEN"
+DB_PATH = "/root/eventbot/eventbot.db"
